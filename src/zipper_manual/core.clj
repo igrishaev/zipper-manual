b70450ca3f97416bc23de0716eb9d334dd3c9c12
Ivan Grishaev <ivan@grishaev.me>
2020-07-17 17:07:37 +0300

    init

---
+++ src/zipper_manual/core.clj
@@ -0,0 +1,7 @@
+(ns zipper-manual.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/zipper_manual/core_test.clj
@@ -0,0 +1,7 @@
+(ns zipper-manual.core-test
+  (:require [clojure.test :refer :all]
+            [zipper-manual.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
